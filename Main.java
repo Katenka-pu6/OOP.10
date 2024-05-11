007579a3d09234afc3192e4b0b1c9c241f0dd36e
Katenka-pu6 <146732153+Katenka-pu6@users.noreply.github.com>
2024-05-11 16:16:37 +0300

    class.student\teacher

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+class Teacher {
+    private int id;
+    private String name;
+
+    public Teacher(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
+class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
